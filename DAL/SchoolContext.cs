d2693f08fa95b431d16e48b94f1fff016c553a8c
chance221 <kelly292@mail.nmc.edu>
2020-02-08 12:36:58 -0500

    first commit

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        //this is the constructor of the database class. It names the database shool context as the string "String Context"
+        public SchoolContext() : base("SchoolContext")
+        {
+      
+        }
+        //Below are the models we created in this class. These are the classes Entity Framework will check to see if they changed. 
+        //If these base models change the default behavior for entity framework is to drop the tabes in the database and then create new ones with the updated structure
+        
+        public DbSet<Student> Students{ get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        //Below is a method that tells entity framework to avoid one its default behaviors and not to change the model names in C# into plural form when they are creating the tables
+        //There are a bunch of default behaviors you can turn on or off to be sure EF behaves the way you need it to but for now this will be the only behavior we will change.
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        //This is the context that the EF will work with but now we need to initialize the database and set up some test data. 
+        //We do this in a class specifically created to initialize or rather start the database. 
+    }
+}
---
+++ DAL/SchoolInitializer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.DAL
+{
+    //The SchoolInitializer class inherits from System.Data.Entity.DropDatabaseIfModelChanges and then is passed in the context that was created earlier. If any of the models
+    //in the School Context change the table will be dropped and a new one created that holds the updated columns. 
+    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        //Notice how the seed class passes the context in as an argument to the seed function. That context is then used to seed the database with the data listed below. 
+        protected override void Seed(SchoolContext context)
+        {
+            base.Seed(context);
+            //creating a list of students
+            var students = new List<Student>
+            {
+                new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+                new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+                new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+            };
+
+            //We are taking the list creted and adding each individually into the database and then saving the changes
+            //this basically creates a new record(or Row)
+            students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+
+
+            //creating a list of Courses
+            var courses = new List<Course>
+            {
+            new Course{CourseID=1050,Title="Chemistry",Credits=3,},
+            new Course{CourseID=4022,Title="Microeconomics",Credits=3,},
+            new Course{CourseID=4041,Title="Macroeconomics",Credits=3,},
+            new Course{CourseID=1045,Title="Calculus",Credits=4,},
+            new Course{CourseID=3141,Title="Trigonometry",Credits=4,},
+            new Course{CourseID=2021,Title="Composition",Credits=3,},
+            new Course{CourseID=2042,Title="Literature",Credits=4,}
+            };
+            //saving that list to the database
+
+            courses.ForEach(s => context.Courses.Add(s));
+            context.SaveChanges();
+
+            var enrollments = new List<Enrollment>
+            {
+            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+            new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
+            new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
+            new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
+            new Enrollment{StudentID=3,CourseID=1050},
+            new Enrollment{StudentID=4,CourseID=1050,},
+            new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
+            new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
+            new Enrollment{StudentID=6,CourseID=1045},
+            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            };
+
+            enrollments.ForEach(s => context.Enrollments.Add(s));
+            context.SaveChanges();
+        }
+        //after the school initializer is created we can now add the context and initializeer to the Web.config flie located at the root of the project and NOT in the views folder
+    }
+}
